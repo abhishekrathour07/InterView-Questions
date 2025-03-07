c4894ca63d04728a04f122c819671fcfc2c37a2a
abhishekrathour07 <singhrathourabhishek7@gmail.com>
2025-03-07 13:53:45 +0530

    otp generator question

---
+++ ques_date_6_march/randomOtp.js
@@ -0,0 +1,11 @@
+
+
+const otpGenerator = (length = 6) => {
+    let otp = "";
+    for (i = 0; i < length; i++) {
+        otp += Math.floor(Math.random() * 10)
+    }
+    console.log("Your One time password is " + otp);
+}
+
+otpGenerator();
