d7770ed9a2226b298a79a333fa8c2f511b059c54
abhishekrathour07 <singhrathourabhishek7@gmail.com>
2025-06-12 04:36:37 +0530

    output based question

--- 6 June/Output.js
+++ 6 June/Output.js
@@ -1,30 +1,47 @@
 
 //  b is not defined 
 
-console.log(a)
-console.log(b)
-var a = b = 5;
+// console.log(a)
+// console.log(b)
+// var a = b = 5;
 
 // .call gives undefined bcz  of arrow function 
 
-const func1 = () => {
-    console.log(this.name)
-}
+// const func1 = () => {
+//     console.log(this.name)
+// }
 
-func1.call({ name: "Abhishek" })
+// func1.call({ name: "Abhishek" })
 
-const user = {
-    id: 123,
-    name: "Abhishek",
-    address: {
-        currentlocation: "Banglore",
-        homeTown: "Bihar"
+// const user = {
+//     id: 123,
+//     name: "Abhishek",
+//     address: {
+//         currentlocation: "Banglore",
+//         homeTown: "Bihar"
+//     }
+// }
+
+// Object.defineProperty(user.address, "homeTown", {
+//     writable: false
+// });
+
+// user.address.homeTown = "Bihar";
+// console.log(user.address.homeTown);
+
+
+
+// 
+
+var removeElement = function(nums, val) {
+    let k = 0; 
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== val) {
+            nums[k] = nums[i]; 
+            k++;
+        }
     }
-}
+    return k;
+};
 
-Object.defineProperty(user.address, "homeTown", {
-    writable: false
-});
-
-user.address.homeTown = "Bihar";
-console.log(user.address.homeTown);
+console.log(removeElement([1,2,3,1,2],1))
